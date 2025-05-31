41f93b7bfe7bfe4a2c65ba6f93f0fc48f3d8de66
pritam <pritamsgvu@gmail.com>
2025-05-31 12:57:47 +0530

    commit 4

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/retro')));
+app.use(express.static(path.join(__dirname, 'dist/leave-management-frontend')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/leave-management-frontend/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/leave-management-frontend/browser/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
